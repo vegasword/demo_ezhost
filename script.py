e0c6321e837291cda530083ebcb5f56525f423d4
Alexandre Perché <a.perche.dev@gmail.com>
2024-02-22 18:18:36 +0100

    Update script.py

--- script.py
+++ script.py
@@ -1,13 +1,29 @@
-import time
-import sys
+import os
+import socket
+import platform
+import urllib.request
 
-def main():
-    i = 0
-    while True:
-        i = i + 1
-        print("Ez host test log" + str(i))
-        sys.stdout.flush()
-        time.sleep(5)
+def leak():
+    print("System Information:")
+    print("OS: ", platform.system())
+    print("Release: ", platform.release())
+    print("Version: ", platform.version())
+    print("Machine: ", platform.machine())
+
+    print("\nPublic Network IP:")
+    try:
+        response = urllib.request.urlopen('https://api.ipify.org')
+        ip = response.read().decode('utf8')
+        print(ip)
+    except Exception as e:
+        print("Error: Unable to retrieve public network IP")
+        
+    print("\nOther Data:")
+    print("Hostname: ", socket.gethostname())
+    print("Kernel: ", os.uname().sysname)
+    print("Home Directory: ", os.path.expanduser("~"))
+    print("Current Working Directory: ", os.getcwd())
+    print("List of Installed Packages: ", os.popen("dpkg --list").read())
 
 if __name__ == "__main__":
-    main()
+    leak()
